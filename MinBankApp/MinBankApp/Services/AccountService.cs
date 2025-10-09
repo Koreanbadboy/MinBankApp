b8b2d18097e160148f2adc45d3508179f1a635da
Ditt namn <din.email@example.com>
2025-10-09 23:17:40 +0200

    ändringar i createraccount

--- MinBankApp/MinBankApp/Services/AccountService.cs
+++ MinBankApp/MinBankApp/Services/AccountService.cs
@@ -6,6 +6,11 @@ namespace MinBankApp.Services;
 public class AccountService : IAccountService
 {
     private readonly List<IBankAccount> _accounts;
+
+    public AccountService()
+    {
+        _accounts = new List<IBankAccount>();
+    }
     public IBankAccount CreateAccount(string name, AccountType accountType, string currency, decimal initialBalance)
     {
         var account = new BankAccount(name, accountType, currency, initialBalance);
